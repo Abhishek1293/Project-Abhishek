14b19eb1bd17af8c7898ae8ce1144537f589653a
Abhishek1293 <148742671+Abhishek1293@users.noreply.github.com>
2023-10-29 20:49:55 +0530

    Update Sales_sample_Task_4.sql

--- Sales_sample_Task_4.sql
+++ Sales_sample_Task_4.sql
@@ -16,25 +16,27 @@ VALUES
     (2, 'West', '2023-04-15', 720.90),
     (3, 'North', '2023-05-05', 1000.25),
     (1, 'East', '2023-05-20', 1150.60);
+	
 SELECT Region, Product_Id, SUM(Sales_Amount) AS Total_Sales
 FROM sales_sample
 GROUP BY Region, Product_Id
 ORDER BY Region, Product_Id;
+
 SELECT Region, SUM(Sales_Amount) AS Total_Sales
 FROM sales_sample
 GROUP BY Region
 UNION ALL
 SELECT 'All Regions' AS Region, SUM(Sales_Amount) AS Total_Sales
 FROM sales_sample;
+
 SELECT Region, Product_Id, Date, SUM(Sales_Amount) AS Total_Sales
 FROM sales_sample
 GROUP BY ROLLUP (Region, Product_Id, Date);
+
 SELECT *
 FROM sales_sample
 WHERE Region = 'East' AND Date BETWEEN '2023-01-01' AND '2023-01-31';
+
 SELECT *
 FROM sales_sample
 WHERE Region = 'West' AND Product_Id = 2 AND Date BETWEEN '2023-01-01' AND '2023-02-28';
-
-
-
