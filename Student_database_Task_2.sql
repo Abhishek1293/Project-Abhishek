c2131435d24fe1276786bdcaea5d86b89fd5b8b4
Abhishek1293 <148742671+Abhishek1293@users.noreply.github.com>
2023-10-29 20:14:16 +0530

    Update Student_database_Task_2.sql

--- Student_database_Task_2.sql
+++ Student_database_Task_2.sql
@@ -1,9 +1,8 @@
--- Create the student_info table
-CREATE TABLE student_info (
-    Student_id INT PRIMARY KEY,
+CREATE TABLE student_table (
+    Student_id INT,
     Stu_name TEXT,
     Department TEXT,
-    email_id TEXT,
+    Email_id TEXT,
     Phone_no NUMERIC,
     Address TEXT,
     Date_of_birth DATE,
@@ -12,38 +11,43 @@ CREATE TABLE student_info (
     GPA NUMERIC,
     Grade TEXT
 );
--- Insert 10 sample records into the student_info table
-INSERT INTO student_info (Student_id, Stu_name, Department, email_id, Phone_no, Address, Date_of_birth, Gender, Major, GPA, Grade)
+
+INSERT INTO student_table (Student_id, Stu_name, Department, Email_id, Phone_no, Address, Date_of_birth, Gender, Major, GPA, Grade)
 VALUES
-    (1, 'John Doe', 'Computer Science', 'john@example.com', 1234567890, '123 Main St', '1995-05-15', 'Male', 'Computer Science', 3.5, 'B'),
-    (2, 'Jane Smith', 'Mathematics', 'jane@example.com', 9876543210, '456 Elm St', '1996-03-22', 'Female', 'Mathematics', 4.0, 'A'),
-    -- Add 8 more sample records here
-    (10, 'Tom Johnson', 'Physics', 'tom@example.com', 5555555555, '789 Oak St', '1994-11-10', 'Male', 'Physics', 4.5, 'A');
--- Retrieve all students' information and sort by grade in descending order
-SELECT * FROM student_info
+(1, 'Amit Kumar', 'Computer Science', 'amit@example.com', 9876543210, '123, ABC Street, Mumbai', '1999-05-15', 'Male', 'Computer Science', 3.8, 'A'),
+(2, 'Priya Sharma', 'Electrical Engineering', 'priya@example.com', 8765432109, '456, XYZ Road, Delhi', '2000-02-28', 'Female', 'Electrical Engineering', 3.6, 'B'),
+(3, 'Rahul Patel', 'Mechanical Engineering', 'rahul@example.com', 7654321098, '789, LMN Avenue, Bangalore', '1998-11-10', 'Male', 'Mechanical Engineering', 3.5, 'B'),
+(4, 'Neha Gupta', 'Biology', 'neha@example.com', 6543210987, '234, PQR Lane, Chennai', '2001-08-05', 'Female', 'Biology', 3.9, 'A'),
+(5, 'Sandeep Singh', 'Physics', 'sandeep@example.com', 5432109876, '567, RST Road, Kolkata', '1997-04-20', 'Male', 'Physics', 3.7, 'B'),
+(6, 'Ananya Reddy', 'Chemistry', 'ananya@example.com', 4321098765, '678, UVW Street, Hyderabad', '2002-03-17', 'Female', 'Chemistry', 3.8, 'A'),
+(7, 'Rajesh Verma', 'Mathematics', 'rajesh@example.com', 3210987654, '890, GHI Avenue, Pune', '1999-07-12', 'Male', 'Mathematics', 3.6, 'B'),
+(8, 'Divya Choudhury', 'Economics', 'divya@example.com', 2109876543, '345, JKL Lane, Jaipur', '2000-10-30', 'Female', 'Economics', 3.4, 'C'),
+(9, 'Rohit Joshi', 'History', 'rohit@example.com', 1098765432, '456, WXY Road, Ahmedabad', '1998-12-03', 'Male', 'History', 3.2, 'C'),
+(10, 'Sonia Mishra', 'Psychology', 'sonia@example.com', 9876543211, '123, ABC Street, Lucknow', '2001-01-25', 'Female', 'Psychology', 3.9, 'A');
+
+SELECT * FROM student_table
 ORDER BY Grade DESC;
--- Retrieve information about all male students
-SELECT * FROM student_info
+
+SELECT * FROM student_table
 WHERE Gender = 'Male';
--- Fetch details of students with GPA less than 5.0
-SELECT * FROM student_info
-WHERE GPA < 5.0;
--- Update the email and grade for a specific student
-UPDATE student_info
-SET email_id = 'new_email@example.com', Grade = 'A'
+
+SELECT * FROM student_table WHERE GPA < 5.0;
+
+UPDATE student_table
+SET Email_id = 'new_email@example.com', Grade = 'A+'
 WHERE Student_id = 1;
--- Retrieve names and ages of students with a grade of "B"
+
 SELECT Stu_name, Date_of_birth
-FROM student_info
-WHERE Grade = 'B';
--- Group by Department and Gender, calculate average GPA
-SELECT Department, Gender, AVG(GPA) AS Avg_GPA
-FROM student_info
-GROUP BY Department, Gender;
--- Rename the table
-ALTER TABLE student_info RENAME TO student_table;
--- Retrieve the name of the student with the highest GPA
-SELECT Stu_name
 FROM student_table
+WHERE Grade = 'B';
+
+SELECT Department, Gender, AVG(GPA) AS Average_GPA
+FROM student_table
+GROUP BY Department, Gender;
+
+ALTER TABLE student_table RENAME TO student_info;
+
+SELECT Stu_name
+FROM student_info
 ORDER BY GPA DESC
 LIMIT 1;
