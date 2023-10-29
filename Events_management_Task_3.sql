2d9cbd8b8f9f359edb95e29fdb8e445790ae3956
Abhishek1293 <148742671+Abhishek1293@users.noreply.github.com>
2023-10-29 20:37:37 +0530

    Update Events_management_Task_3.sql

--- Events_management_Task_3.sql
+++ Events_management_Task_3.sql
@@ -1,4 +1,3 @@
--- Create the Events table
 CREATE TABLE Events (
     Event_Id INT PRIMARY KEY,
     Event_Name VARCHAR(255),
@@ -7,7 +6,6 @@ CREATE TABLE Events (
     Event_Description TEXT
 );
 
--- Create the Attendees table
 CREATE TABLE Attendees (
     Attendee_Id INT PRIMARY KEY,
     Attendee_Name VARCHAR(255),
@@ -16,7 +14,6 @@ CREATE TABLE Attendees (
     Attendee_City VARCHAR(255)
 );
 
--- Create the Registrations table with foreign key constraints
 CREATE TABLE Registrations (
     Registration_Id INT PRIMARY KEY,
     Event_Id INT,
@@ -26,40 +23,55 @@ CREATE TABLE Registrations (
     FOREIGN KEY (Event_Id) REFERENCES Events(Event_Id),
     FOREIGN KEY (Attendee_Id) REFERENCES Attendees(Attendee_Id)
 );
--- Insert sample data into the Events table
+
 INSERT INTO Events (Event_Id, Event_Name, Event_Date, Event_Location, Event_Description)
 VALUES
-    (4, 'Conference A', '2023-11-15', 'City Center Convention Center', 'Annual tech conference'),
-    (5, 'Seminar B', '2023-12-10', 'Downtown Hotel', 'Marketing strategies seminar');
+    (1, 'Music Concert', '2023-11-15', 'Concert Hall', 'An amazing music concert'),
+    (2, 'Tech Conference', '2023-12-10', 'Tech Center', 'A conference for tech enthusiasts'),
+    (3, 'Art Exhibition', '2023-10-20', 'Art Gallery', 'A showcase of contemporary art'),
+    (4, 'Sports Tournament', '2023-11-30', 'Sports Arena', 'Exciting sports competition'),
+    (5, 'Food Festival', '2023-12-05', 'City Park', 'A celebration of culinary delights');
 	
 INSERT INTO Attendees (Attendee_Id, Attendee_Name, Attendee_Phone, Attendee_Email, Attendee_City)
 VALUES
-    (1, 'John Doe', '123-456-7890', 'john@example.com', 'New York'),
-    (2, 'Jane Smith', '987-654-3210', 'jane@example.com', 'Los Angeles');
+    (1, 'John Doe', '555-123-4567', 'john.doe@example.com', 'New York'),
+    (2, 'Jane Smith', '555-987-6543', 'jane.smith@example.com', 'Los Angeles'),
+    (3, 'Robert Johnson', '555-555-5555', 'robert.johnson@example.com', 'Chicago'),
+    (4, 'Emily Davis', '555-888-9999', 'emily.davis@example.com', 'Houston'),
+    (5, 'Michael Brown', '555-777-3333', 'michael.brown@example.com', 'San Francisco');
+	
 INSERT INTO Registrations (Registration_Id, Event_Id, Attendee_Id, Registration_Date, Registration_Amount)
 VALUES
-    (1, 1, 1, '2023-11-05', 150.00),
-    (2, 1, 2, '2023-11-06', 150.00),
-    (3, 2, 1, '2023-12-01', 100.00);
+    (1, 1, 1, '2023-11-01', 50.00),
+    (2, 2, 2, '2023-11-05', 100.00),
+    (3, 3, 3, '2023-10-15', 25.00),
+    (4, 4, 4, '2023-11-20', 75.00),
+    (5, 5, 5, '2023-12-01', 30.00);
+
 INSERT INTO Events (Event_Id, Event_Name, Event_Date, Event_Location, Event_Description)
-VALUES (3, 'Workshop C', '2023-12-20', 'Community Center', 'Hands-on coding workshop');
-UPDATE Events
-SET Event_Description = 'Updated event description'
+VALUES (6, 'New Year Eve Party', '2023-12-31', 'Downtown Club', 'Ring in the new year with us!');
+
+UPDATE Events SET Event_Location = 'Updated Location'
 WHERE Event_Id = 1;
-DELETE FROM Registrations WHERE Event_Id = 2;
-DELETE FROM Events WHERE Event_Id = 2;
+
+DELETE FROM Events WHERE Event_Id = 6;
+
 INSERT INTO Attendees (Attendee_Id, Attendee_Name, Attendee_Phone, Attendee_Email, Attendee_City)
-VALUES (3, 'Bob Johnson', '555-555-5555', 'bob@example.com', 'Chicago');
-INSERT INTO Registrations (Event_Id, Attendee_Id, Registration_Date, Registration_Amount)
-VALUES (1, 3, '2023-11-07', 150.00);
+VALUES (6, 'Alice Johnson', '555-333-4444', 'alice.johnson@example.com', 'Miami');
+
+INSERT INTO Registrations (Registration_Id, Event_Id, Attendee_Id, Registration_Date, Registration_Amount)
+VALUES (6, 1, 6, '2023-11-10', 60.00);
+
 SELECT * FROM Events;
+SELECT A.Attendee_Name, A.Attendee_Phone, A.Attendee_Email, A.Attendee_City
+FROM Attendees A
+
 SELECT A.Attendee_Name, A.Attendee_Phone, A.Attendee_Email, A.Attendee_City
 FROM Attendees A
 JOIN Registrations R ON A.Attendee_Id = R.Attendee_Id
 WHERE R.Event_Id = 1;
+
 SELECT E.Event_Name, COUNT(R.Attendee_Id) AS Attendee_Count, SUM(R.Registration_Amount) AS Total_Revenue
 FROM Events E
 LEFT JOIN Registrations R ON E.Event_Id = R.Event_Id
 GROUP BY E.Event_Name;
-
-
